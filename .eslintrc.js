e099948b06adc245a72a760699e584f8c44513bd
O Teerasak Vichadee <iolumin@gmail.com>
2021-07-01 13:54:13 +0700

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
+  plugins: ['prettier', 'jest', 'cypress', 'testing-library', 'jest-dom'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'testing-library/no-render-in-setup': 'error',
+    'testing-library/no-wait-for-empty-callback': 'error',
+    'testing-library/prefer-explicit-assert': 'error',
+    'testing-library/prefer-presence-queries': 'error',
+    'testing-library/prefer-screen-queries': 'error',
+    'testing-library/prefer-wait-for': 'error',
+  },
+}
